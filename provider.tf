109b38dbfbcc134f8011bcf16ad898f4a4484106
1998arunrekha <arunar16061998@gmail.com>
2022-09-07 19:42:57 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.21.1"
+    }
+  }
+}
+
+# Provider-1 for EastUS (Default Provider)
+provider "azurerm" {
+  features {}
+}
+
+# Provider-2 for WestUS
+provider "azurerm" {
+  features {
+    virtual_machine {
+      delete_os_disk_on_deletion = false # This will ensure when the Virtual Machine is destroyed, Disk is not deleted, default is true and we can alter it at provider level
+    }
+  }
+  alias = "provider2-westus"
+  #client_id = 
+  #client_secret = 
+  #environment = 
+  #subscription_id =
+}
