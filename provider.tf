3af3d5775c4a1e7683654af0f9a840790e3118f7
1998arunrekha <arunar16061998@gmail.com>
2022-09-07 19:54:45 +0530

    second commit

--- provider.tf
+++ provider.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
 provider "azurerm" {
   features {
     virtual_machine {
-      delete_os_disk_on_deletion = false # This will ensure when the Virtual Machine is destroyed, Disk is not deleted, default is true and we can alter it at provider level
+     # delete_os_disk_on_deletion = false # This will ensure when the Virtual Machine is destroyed, Disk is not deleted, default is true and we can alter it at provider level
     }
   }
   alias = "provider2-westus"
